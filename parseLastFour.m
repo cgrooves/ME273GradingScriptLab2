8b7a79640cf93b7f2c7e240836bfaae5d9af16bc
cgrooves <caleb.j.groves@gmail.com>
2018-01-17 21:35:51 -0700

    First run; basic parse from 1st underscore found in filename passed in

---
+++ parseLastFour.m
@@ -0,0 +1,41 @@
+%--------------------------------------------------------------
+% FILE: 
+% AUTHOR: Caleb Groves
+% DATE: 1/17/18
+% 
+% PURPOSE: Parse the name of a student's file submission in order to
+% extract the last four digits of their BYU ID.
+%
+%
+% INPUT: String containing the filename to parse.
+%
+%
+% OUTPUT: Last four digits of student's BYU ID, if contained after the
+% first underscore in the filename. Otherwise, returns the string 'ERROR'.
+%
+%
+% NOTES: 
+%
+%
+% VERSION HISTORY
+% V1 - 
+% V2 - 
+% V3 - 
+% 
+%--------------------------------------------------------------
+function last4 = parseLastFour(filename)
+
+    % start going through each letter in the filename
+    for i = 1:length(filename)
+       
+        % check to see if letter is underscore
+        if filename(i) == '_'
+            last4 = str2num(filename(i+1:i+3));
+            return;
+        end
+    end
+    
+    % return ERROR
+    last4 = 'ERROR';
+
+end
